b42ec5b252113638508225170d5c5d24888b6633
ron34353435 <ron34353435@gmail.com>
2023-11-12 21:43:44 +0200

    add documentation

--- backend/deploy.js
+++ backend/deploy.js
@@ -1,69 +1,59 @@
-const deployContract = require('./methods/deployContract')
-const deployMetadata = require('./methods/deployMetadata')
-const verifyContract = require('./methods/verifyContract')
-const fs = require("fs");
-const hre = require("hardhat");
-require('dotenv').config()
+const deployContract = require('./methods/deployContract');
+const deployMetadata = require('./methods/deployMetadata');
+const verifyContract = require('./methods/verifyContract');
+const fs = require('fs');
+const hre = require('hardhat');
+require('dotenv').config();
 
-const CONTRACT_NAME = "Freediving Animal";
-const CONTRACT_SYMBOL="FDA";
-const DESCRIPTION = "Freediving animal";
-const MAX_SUPPLY = 30
-const IMAGE_URI = "https://i.ibb.co/0DY8JRM/freediving-Animal.png";
+const CONTRACT_NAME = 'Freediving Animal';
+const CONTRACT_SYMBOL = 'FDA';
+const DESCRIPTION = 'Freediving animal';
+const MAX_SUPPLY = 30;
+const IMAGE_URI = 'https://i.ibb.co/0DY8JRM/freediving-Animal.png';
 const INITIAL_OWNER = process.env.WALLET_PUBLIC_KEY;
 
-
-
 // Deploy metadata to nft.storage
-async function metadata(contractName, description, imageURI) {
+async function uploadMetadata(contractName, description, imageURI) {
     try {
         const uri = await deployMetadata(contractName, description, imageURI);
         console.log('Contract Metadata Uploaded Successfully');
         return uri;
+    } catch (error) {
+        throw new Error(`Metadata upload failed for ${contractName}: ${error.message}`);
     }
-    catch(e) {
-        throw new Error(`ERROR: Metadata upload failed for ${contractName} with: ${e.message}`);
-    }
-}  
+}
 
 // Compile the contract with Hardhat
-async function compile(contractName) {
+async function compileContract(contractName) {
     try {
-        await hre.run("compile");
-    }
-    catch(e) {
-        throw new Error(`ERROR: Contract compilation failed for ${contractName} with: ${e.message}`);
+        await hre.run('compile');
+    } catch (error) {
+        throw new Error(`Contract compilation failed for ${contractName}: ${error.message}`);
     }
 }
 
 // Deploy the contract to the blockchain
-async function deploy(contractSymbol, contractInitURI, maxSupply) {
+async function deployContractToBlockchain(symbol, initialURI, supply) {
     try {
-        const contractAddress = await deployContract(contractSymbol, contractInitURI, maxSupply, INITIAL_OWNER);
-        return(contractAddress);
-    }
-    catch(e) {
-        throw new Error(`ERROR: Contract upload failed for ${CONTRACT_NAME} with: ${e.message}`);
+        const contractAddress = await deployContract(symbol, initialURI, supply, INITIAL_OWNER);
+        return contractAddress;
+    } catch (error) {
+        throw new Error(`Contract deployment failed for ${CONTRACT_NAME}: ${error.message}`);
     }
 }
 
-    // Issue the company as stock on the blockchain
+// Main deployment function
 async function main() {
     try {
-        console.log(`Start deployment of contract: ${CONTRACT_NAME}`);
-        // Deploy metadata
-        contractInitURI = await metadata(CONTRACT_NAME, DESCRIPTION, IMAGE_URI);
-        // Compile the contract
-        await compile(CONTRACT_NAME);
-        // Deploy contract
-        contractAddress = await deploy(CONTRACT_SYMBOL, contractInitURI, MAX_SUPPLY);
+        console.log(`Starting deployment of contract: ${CONTRACT_NAME}`);
+        const contractInitialURI = await uploadMetadata(CONTRACT_NAME, DESCRIPTION, IMAGE_URI);
+        await compileContract(CONTRACT_NAME);
+        const deployedContractAddress = await deployContractToBlockchain(CONTRACT_SYMBOL, contractInitialURI, MAX_SUPPLY);
         await new Promise(resolve => setTimeout(resolve, 20000));
-        // Verify contract
-        await verifyContract(contractAddress, CONTRACT_NAME, CONTRACT_SYMBOL, contractInitURI, MAX_SUPPLY, INITIAL_OWNER);
-        return(contractAddress);
-    }
-    catch(e){
-        console.error(e);
+        await verifyContract(deployedContractAddress, CONTRACT_NAME, CONTRACT_SYMBOL, contractInitialURI, MAX_SUPPLY, INITIAL_OWNER);
+        console.log(`Contract deployed at address: ${deployedContractAddress}`);
+    } catch (error) {
+        console.error(`Deployment error: ${error.message}`);
     }
 }
 
