1b23f58d73b5e808245d308fd2e8ef6d0876ad1a
ron34353435 <ron34353435@gmail.com>
2023-11-12 21:34:33 +0200

    make the website look nicer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,17 @@
 import React, { useState } from "react";
-import CssBaseline from "@mui/material/CssBaseline";
-import Container from "@mui/material/Container";
-import Paper from "@mui/material/Paper";
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Grid from "@mui/material/Grid";
-import Button from "@mui/material/Button";
+import { CssBaseline, Container, Paper, Typography, AppBar, Toolbar, Grid, Button, CircularProgress } from "@mui/material";
 import { BrowserProvider, Contract } from "ethers";
 import metamasklogo from "./metamask.png";
 
-
 function App() {
-
   const [provider, setProvider] = useState(null);
   const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const CONTRACT_ADDRESS = "0xAef9Ded328FD853C2946F5894d6fA8830A29fc0D";
   const CONTRACT_ABI = [{"inputs":[{"internalType":"string","name":"initURI","type":"string"},{"internalType":"uint256","name":"initMaxSupply","type":"uint256"},{"internalType":"address","name":"initialOwner","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[{"internalType":"address","name":"sender","type":"address"},{"internalType":"uint256","name":"balance","type":"uint256"},{"internalType":"uint256","name":"needed","type":"uint256"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"ERC1155InsufficientBalance","type":"error"},{"inputs":[{"internalType":"address","name":"approver","type":"address"}],"name":"ERC1155InvalidApprover","type":"error"},{"inputs":[{"internalType":"uint256","name":"idsLength","type":"uint256"},{"internalType":"uint256","name":"valuesLength","type":"uint256"}],"name":"ERC1155InvalidArrayLength","type":"error"},{"inputs":[{"internalType":"address","name":"operator","type":"address"}],"name":"ERC1155InvalidOperator","type":"error"},{"inputs":[{"internalType":"address","name":"receiver","type":"address"}],"name":"ERC1155InvalidReceiver","type":"error"},{"inputs":[{"internalType":"address","name":"sender","type":"address"}],"name":"ERC1155InvalidSender","type":"error"},{"inputs":[{"internalType":"address","name":"operator","type":"address"},{"internalType":"address","name":"owner","type":"address"}],"name":"ERC1155MissingApprovalForAll","type":"error"},{"inputs":[],"name":"EnforcedPause","type":"error"},{"inputs":[],"name":"ExpectedPause","type":"error"},{"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"OwnableInvalidOwner","type":"error"},{"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"OwnableUnauthorizedAccount","type":"error"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"account","type":"address"},{"indexed":true,"internalType":"address","name":"operator","type":"address"},{"indexed":false,"internalType":"bool","name":"approved","type":"bool"}],"name":"ApprovalForAll","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"account","type":"address"}],"name":"Paused","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"operator","type":"address"},{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256[]","name":"ids","type":"uint256[]"},{"indexed":false,"internalType":"uint256[]","name":"values","type":"uint256[]"}],"name":"TransferBatch","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"operator","type":"address"},{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"id","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"TransferSingle","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"string","name":"value","type":"string"},{"indexed":true,"internalType":"uint256","name":"id","type":"uint256"}],"name":"URI","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"account","type":"address"}],"name":"Unpaused","type":"event"},{"inputs":[{"internalType":"address","name":"account","type":"address"},{"internalType":"uint256","name":"id","type":"uint256"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address[]","name":"accounts","type":"address[]"},{"internalType":"uint256[]","name":"ids","type":"uint256[]"}],"name":"balanceOfBatch","outputs":[{"internalType":"uint256[]","name":"","type":"uint256[]"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"id","type":"uint256"}],"name":"exists","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"id","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"account","type":"address"},{"internalType":"address","name":"operator","type":"address"}],"name":"isApprovedForAll","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"maxSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"mint","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"pause","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"paused","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256[]","name":"ids","type":"uint256[]"},{"internalType":"uint256[]","name":"values","type":"uint256[]"},{"internalType":"bytes","name":"data","type":"bytes"}],"name":"safeBatchTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"id","type":"uint256"},{"internalType":"uint256","name":"value","type":"uint256"},{"internalType":"bytes","name":"data","type":"bytes"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"operator","type":"address"},{"internalType":"bool","name":"approved","type":"bool"}],"name":"setApprovalForAll","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"newSupply","type":"uint256"}],"name":"setMaxSupply","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"newuri","type":"string"}],"name":"setURI","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes4","name":"interfaceId","type":"bytes4"}],"name":"supportsInterface","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"id","type":"uint256"}],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"unpause","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"uri","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"}];
-  
-  const connectMetamask = async ({ address }) => {
+
+  const connectMetamask = async () => {
     if (window.ethereum) {
       try {
         await window.ethereum.request({ method: "eth_requestAccounts" });
@@ -28,6 +19,7 @@ function App() {
         setProvider(provider);
       } catch (error) {
         console.error(error);
+        setErrorMessage("Error connecting to Metamask: " + error.message);
       }
     } else {
       setErrorMessage(
@@ -37,14 +29,12 @@ function App() {
   };
 
   const mintToken = async (event) => {
-    
-    event.preventDefault()
-
+    event.preventDefault();
     if (!provider) return;
-  
+
+    setLoading(true);
     try {
       const signer = await provider.getSigner();
-      console.log(signer)
       const contract = new Contract(CONTRACT_ADDRESS, CONTRACT_ABI, signer);
       const tx = await contract.mint();
       await tx.wait();
@@ -52,66 +42,50 @@ function App() {
     } catch (error) {
       console.error("Error minting token:", error);
       setErrorMessage("Minting failed: " + error.message);
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
     <React.Fragment>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">
+            DApp Demo Application
+          </Typography>
+        </Toolbar>
+      </AppBar>
 
-        <AppBar position="static">
-            <Toolbar>
-                <Typography variant="h6">
-                    DApp Demo Application
-                </Typography>
-            </Toolbar>
-        </AppBar>
-
-      
       <CssBaseline />
       <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
-        <Paper
-          variant="outlined"
-          sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}
-        >
+        <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
           <Typography component="h1" variant="h4" align="center" sx={{ mb: 5 }}>
             Mint Page
           </Typography>
           <form className="form flex-col">
-            <Grid container spacing={10}>
-              <Grid item xs={12}>
-                <Typography variant="h6" gutterBottom>
-                  Connect Metamask Wallet
-                </Typography>
+            <Grid container spacing={3}>
+              <Grid item xs={12} textAlign="center">
+                <Button varient="contained" onClick={connectMetamask} disabled={provider != null} >
+                  <img src={metamasklogo} width="64" height="64" alt="metamask-logo" />
+                  {provider ? "Connected" : "Connect to Metamask"}
+                </Button>
+                {errorMessage && <Typography color="error">{errorMessage}</Typography>}
               </Grid>
-              <Grid item xs={12} >
-                <Box display="flex" justifyContent="center">
-                <div>
-                  {provider ? (
-                    <Button varient="contained" disabled>
-                      <img src={metamasklogo} width="64" height="64" alt="metamask-logo" />
-                      Connected
-                    </Button>
-                  ) : (
-                    <Button varient="contained" onClick={connectMetamask}>
-                      <img src={metamasklogo} width="64" height="64" alt="metamask-logo" />
-                      Connect to Metamask
-                    </Button>
-                  )}
-                  <p>{errorMessage}</p>
-                </div>
-                </Box>
+              <Grid item xs={12} textAlign="center">
+                {loading && <CircularProgress />}
               </Grid>
-            </Grid>
-            <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
+              <Grid item xs={12} textAlign="center">
                 <Button
                   type="submit"
                   variant="contained"
-                  sx={{ mt: 3, ml: 1 }}
-                  onClick={(event) => mintToken(event)}
+                  disabled={loading}
+                  onClick={mintToken}
                 >
                   Mint Token!
                 </Button>
-              </Box>
+              </Grid>
+            </Grid>
           </form>
         </Paper>
       </Container>
